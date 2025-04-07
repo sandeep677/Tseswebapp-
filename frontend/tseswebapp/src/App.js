f8ea4e00fc46a1d06f2eafd93785c21226960195
sandeep677 <106852000+sandeep677@users.noreply.github.com>
2025-04-07 10:27:52 +0545

    Update App.js

--- frontend/tseswebapp/src/App.js
+++ frontend/tseswebapp/src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navigation from '../src/components/Navigation.js';
-import Home from '../src/pages/Home';
-import Notice from '../src/pages/Notice';
-import Attendance from '../src/pages/Attendance';
-import Homework from '../src/pages/Homework';
-import Login from '../src/pages/Login';
-import ChangePassword from '../src/components/Changepassword.js';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation'; // Importing Navigation component
+import Home from './pages/Home'; // Importing the Home page component
+import Notice from './pages/Notice'; // Importing the Notice page component
+import Attendance from './pages/Attendance'; // Importing the Attendance page component
+import Homework from './pages/Homework'; // Importing the Homework page component
+import Login from './pages/Login'; // Importing the Login page component
+import ChangePassword from './components/Changepassword'; // Importing the ChangePassword component
+import './App.css'; // Importing CSS file
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/notice" element={<Notice />} />
-          <Route path="/attendance" element={<Attendance />} />
-          <Route path="/homework" element={<Homework />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-        </Routes>
-      </main>
-    </div>
+    <Router> {/* Router wraps the entire app to handle routing */}
+      <div className="App">
+        <Navigation /> {/* Render the Navigation component */}
+        <main className="container">
+          <Routes>
+            {/* Define routes for different pages */}
+            <Route path="/" element={<Home />} />
+            <Route path="/notice" element={<Notice />} />
+            <Route path="/attendance" element={<Attendance />} />
+            <Route path="/homework" element={<Homework />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/change-password" element={<ChangePassword />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
